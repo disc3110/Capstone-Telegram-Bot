7d91a756d71fd7f2a9992fbad2a9ed532e0e8e8f
Diego Solis <>
2021-03-01 16:03:13 -0600

    Update quiz_spec.rb

---
+++ spec/quiz_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../lib/quiz'
+
+describe Quiz do
+  describe '#give_question' do
+    it "gets a random question from an array of questions" do
+      expect().to
+    end
+  end
+  describe '#give_options' do
+  end
+  describe '#give_answer' do
+  end
+end
